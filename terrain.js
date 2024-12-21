8aaa86eb9f9bd2a05b2e75a4a57976946f3d20ea
k01756ws <william.shedden@student.manchester.ac.uk>
2024-12-21 00:41:23 +0000

    Add fertility based on terrain type

--- terrain.js
+++ terrain.js
@@ -7,11 +7,23 @@ function generateTerrain() {
     // Adjust the noise thresholds to make water more common
     if (noiseValue < 0.3) { // Increase the threshold for water
       hex.type = 'water';
+      hex.fertility = 0; // Water has 0 fertility
       claimableTiles.delete(hex.getKey()); // Remove water tiles from claimable tiles
-    } else if (noiseValue < 0.4) hex.type = 'grass';
-    else if (noiseValue < 0.6) hex.type = 'desert';
-    else if (noiseValue < 0.7) hex.type = 'mountain'; // Mountain tiles
-    else if (noiseValue < 0.8) hex.type = 'forest';
-    else hex.type = 'snow';
+    } else if (noiseValue < 0.4) {
+      hex.type = 'grass';
+      hex.fertility = 0.8; // Grass has high fertility
+    } else if (noiseValue < 0.6) {
+      hex.type = 'desert';
+      hex.fertility = 0.1; // Desert has very low fertility
+    } else if (noiseValue < 0.7) {
+      hex.type = 'mountain'; // Mountain tiles
+      hex.fertility = 0.3; // Mountain has low fertility
+    } else if (noiseValue < 0.8) {
+      hex.type = 'forest';
+      hex.fertility = 0.9; // Forest has very high fertility
+    } else {
+      hex.type = 'snow';
+      hex.fertility = 0.1; // Snow has very low fertility
+    }
   });
 }
