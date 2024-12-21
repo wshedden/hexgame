449265370063528964f639c2e3105b246e47c80d
k01756ws <william.shedden@student.manchester.ac.uk>
2024-12-21 00:14:10 +0000

    Concise coordinates

--- panel.js
+++ panel.js
@@ -93,15 +93,39 @@ class PanelManager {
             ];
         });
 
-        this.createPanel(0, 0, 200, 'Player 1 Hexes', () => [
-            `Occupied Hexes: ${players[0].occupiedHexes.length}`,
-            ...players[0].occupiedHexes.map((hex, i) => `Hex ${i}: (${hex.q}, ${hex.r})`)
-        ], { rightAligned: true });
+        this.createPanel(0, 0, 200, 'Player 1 Hexes', () => {
+            let lines = [`Occupied Hexes: ${players[0].occupiedHexes.length}`];
+            for (let i = 0; i < players[0].occupiedHexes.length; i += 3) {
+                let hex1 = players[0].occupiedHexes[i];
+                let hex2 = players[0].occupiedHexes[i + 1];
+                let hex3 = players[0].occupiedHexes[i + 2];
+                if (hex3) {
+                    lines.push(`${hex1.q}, ${hex1.r}    ${hex2.q}, ${hex2.r}    ${hex3.q}, ${hex3.r}`);
+                } else if (hex2) {
+                    lines.push(`${hex1.q}, ${hex1.r}    ${hex2.q}, ${hex2.r}`);
+                } else {
+                    lines.push(`${hex1.q}, ${hex1.r}`);
+                }
+            }
+            return lines;
+        }, { rightAligned: true });
 
-        this.createPanel(0, 0, 200, 'Player 2 Hexes', () => [
-            `Occupied Hexes: ${players[1].occupiedHexes.length}`,
-            ...players[1].occupiedHexes.map((hex, i) => `Hex ${i}: (${hex.q}, ${hex.r})`)
-        ], { rightAligned: true });
+        this.createPanel(0, 0, 200, 'Player 2 Hexes', () => {
+            let lines = [`Occupied Hexes: ${players[1].occupiedHexes.length}`];
+            for (let i = 0; i < players[1].occupiedHexes.length; i += 3) {
+                let hex1 = players[1].occupiedHexes[i];
+                let hex2 = players[1].occupiedHexes[i + 1];
+                let hex3 = players[1].occupiedHexes[i + 2];
+                if (hex3) {
+                    lines.push(`${hex1.q}, ${hex1.r}    ${hex2.q}, ${hex2.r}    ${hex3.q}, ${hex3.r}`);
+                } else if (hex2) {
+                    lines.push(`${hex1.q}, ${hex1.r}    ${hex2.q}, ${hex2.r}`);
+                } else {
+                    lines.push(`${hex1.q}, ${hex1.r}`);
+                }
+            }
+            return lines;
+        }, { rightAligned: true });
 
         this.createPanel(0, 0, 200, 'Selected Unit', () => [
             `Selected Unit: ${selectedUnitType}`
