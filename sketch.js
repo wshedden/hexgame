a8a8484b5f550dbaaa7b65305c33f479788ea763
k01756ws <william.shedden@student.manchester.ac.uk>
2024-12-22 11:58:19 +0000

    Change keybinds

--- sketch.js
+++ sketch.js
@@ -62,7 +62,6 @@ function initialiseTerrainColours() {
   registerTerrainType('forest', color(34, 139, 34));
   registerTerrainType('snow', color(255, 250, 250));
 }
-
 function keyPressed() {
   if (currentState === GameState.INIT) {
     if (key === 't' || key === 'T') {
@@ -71,13 +70,13 @@ function keyPressed() {
     } else {
       setState(GameState.PLAYING);
     }
-  } else if (key === ' ') { // Check if the space key is pressed
+  } else if (key === 'p' || key === 'P') { // Pause the game with 'p'
     if (currentState === GameState.PLAYING) {
       setState(GameState.PAUSED);
     } else if (currentState === GameState.PAUSED) {
       setState(GameState.PLAYING);
     }
-  } else if (key === 'p' || key === 'P') { // Check if the 'P' key is pressed
+  } else if (key === "'") { // Switch player with '
     switchPlayer();
   } else if (keyCode === LEFT_ARROW) {
     speedMultiplier = max(0.1, speedMultiplier / 1.1); // Decrease speed, minimum 0.1x
@@ -87,7 +86,7 @@ function keyPressed() {
     selectedUnitType = 'settler';
   } else if (key === '2') {
     selectedUnitType = 'soldier';
-  } else if (key === 'k' || key === 'K') { // Toggle pathfinding mode
+  } else if (key === 'x' || key === 'X') { // Toggle pathfinding mode with 'x'
     pathfindingMode = !pathfindingMode;
     if (pathfindingMode) {
       print("Pathfinding mode activated.");
